8b0f17f11e3571af7add592e44e96d80b8678328
Eric Patterson <erpat@Window-To-Erics-Soul.(none)>
2017-07-29 15:58:52 -0500

    update with franklins changes

--- server/index.js
+++ server/index.js
@@ -5,17 +5,21 @@ const request = require('request')
 const session = require('express-session')
 const massive = require('massive')
 const passport = require('passport')
+var google = require('googleapis')
+const oAuth = require('passport-oauth')
 const Auth0Strategy = require('passport-auth0')
 const config = require('./config')
+const Dfp = require('node-google-dfp')
 
 
 //========================== Setup ================================
 
 // express setup
 let app = module.exports = express()
-app.use(express.static('dist'))
+app.use(express.static('./dist'))
 app.use(bodyParser.json())
 app.use(cors())
+
 
 const massiveInstance = massive.connectSync({
   connectionString: config.dev.database.connectionString
@@ -33,6 +37,38 @@ app.use(session({
   saveUninitialized:false
 }))
 
+//========================== OAuth2 ================================
+
+
+var OAuth2 = google.auth.OAuth2;
+
+var oauth2Client = new OAuth2(
+  YOUR_CLIENT_ID,
+  YOUR_CLIENT_SECRET,
+  YOUR_REDIRECT_URL
+);
+
+// generate a url that asks permissions for Google+ and Google Calendar scopes
+var scopes = [
+  'https://www.googleapis.com/auth/plus.me',
+  'https://www.googleapis.com/auth/calendar'
+];
+
+var url = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  access_type: 'offline',
+
+  // If you only need one scope you can pass it as a string
+  scope: scopes,
+
+  // Optional property that passes state parameters to redirect URI
+  // state: { foo: 'bar' }
+})
+
+
+//========================== Auth 0 ================================
+
+
 // initialize passport and Auth0
 app.use(passport.initialize())
 app.use(passport.session())
@@ -42,7 +78,7 @@ passport.use(new Auth0Strategy(config.dev.auth0Strategy, function(accessToken, r
 
 // login endpoint
 app.get('/login', passport.authenticate('auth0'), function(req,res) {
-  res.redirect('/#!/space')
+  res.redirect('/#!/workouts')
 })
 
 app.get('/auth/callback',
@@ -59,15 +95,43 @@ passport.deserializeUser(function(user,done) {
   done(null, user)
 })
 
-// import controller module
-const controller = require('./servCtrl')
-
-//========================== Endpoints ================================
-
 // Get user info from Auth0
 app.get('/api/auth0', function(req,res) {
   res.send(req.user)
 })
+
+//========================== Google DPF ================================
+
+
+   let dfpUser = new Dfp.User(NETWORK_CODE, APP_NAME, VERSION)
+
+
+    dfpUser.setSettings({
+        client_id : config.dev.dfpSettings.clientID,
+        client_secret : config.dev.dfpSettings.clientSecret,
+        refresh_token : config.dev.dfpSettings.refreshToken,
+        redirect_url : config.dev.dfpSettings.redirectUrl
+      })
+      
+
+    dfpUser.getService('LineItemService', function (err, lineItemService) {
+      if (err) {
+        return console.error(err);
+      }
+
+      var statement = new DfpClass.Statement('WHERE id = 103207340');
+
+      lineItemService.getLineItemsByStatement(statement, function (err, results) {
+        console.log(results);
+      })
+    })
+
+
+// import controller module
+// const controller = require('./servCtrl')
+
+//========================== Endpoints ================================
+
 
 
 //========================== Listen ================================
